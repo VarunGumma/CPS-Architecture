75e9848b797bddd8671978ebdb1a1321bc4ba891
Varun Gumma <45076943+VarunGumma@users.noreply.github.com>
2021-04-05 23:10:44 +0530

    Add files via upload

---
+++ CPS/proxy_server/proxy_server.py
@@ -0,0 +1,42 @@
+import socket
+from os.path import exists
+
+
+def read_file(fname):
+    with open(fname, 'r') as content_file:
+        data = content_file.read()
+        content_file.close()
+    return data
+
+print("[PROXY SERVER]")
+static_server_port = 44672
+static_proxy_server_port = 44671
+max_data_to_be_recv = 10240
+
+my_sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sckt.bind(("127.0.0.1", static_proxy_server_port))
+my_sckt.listen(5)
+
+conn, _ = my_sckt.accept()
+fname = conn.recv(10).decode()
+print(f"Filename received: {fname}")
+print("Searching for file in proxy-server directory.")
+
+if not exists(fname):
+    print("File not found. Connecting to server.")
+    temp_sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    temp_sckt.connect(("127.0.0.1", static_server_port))
+    temp_sckt.send(fname.encode())
+    data = temp_sckt.recv(max_data_to_be_recv)
+    with open(fname, 'w') as content_file:
+        content_file.write(data.decode())
+        content_file.close()
+    temp_sckt.close()
+    print("File received. Transmitting to client")
+else:
+    print("File found. Transmitting it to client.")
+
+data = read_file(fname)
+conn.send(data.encode())
+my_sckt.close()
+
---
+++ CPS/server/server.py
@@ -0,0 +1,27 @@
+import socket
+
+
+def read_file(fname):
+    with open(fname, 'r') as content_file:
+        data = content_file.read()
+        content_file.close()
+    return data
+
+print("[SERVER]")
+static_server_port = 44672
+my_sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sckt.bind(("127.0.0.1", static_server_port))
+my_sckt.listen(5)
+
+conn, _ = my_sckt.accept()
+fname = conn.recv(10).decode()
+print(f"Filename received: {fname}")
+print("Searching for file in server directory.")
+print("File found. Transmitting file.")
+data = read_file(fname)
+conn.send(data.encode())
+my_sckt.close()
+    
+
+ 
+
